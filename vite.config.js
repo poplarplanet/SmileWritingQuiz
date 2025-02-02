120cc5664d519d4ed6d2f5e848ba1629c1ad3337
oceanooooo <funboard6287@gmail.com>
2025-02-03 05:24:15 +0900

    Revert "Resolve conflict: Keep vite.config.js"
    This reverts commit 070005bded3e59315d8569659b746afa593bcc04.

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/SmileWritingQuiz/', // GitHub Pages의 레포지토리 이름
-});
