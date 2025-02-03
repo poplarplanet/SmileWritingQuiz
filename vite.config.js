9d5d6d59618b3945d7bbca49cfc68b1eb4b84e9a
oceanooooo <funboard6287@gmail.com>
2025-02-03 10:59:13 +0900

    Update vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  base: "/SmileWritingQuiz/",
+  server: {
+    port: 3000
+  },
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    sourcemap: true,
+    rollupOptions: {
+      input: {
+        main: "./index.html"
+      }
+    }
+  }
+});
